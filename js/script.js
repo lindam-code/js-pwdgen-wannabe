f23e1cbf6259d5a96ca0e0bf7e8da8f329d25b4f
lindam <linda.minotti2@gmail.com>
2020-05-19 15:29:30 +0200

    Tolti comandi di debug per visualizzazione in console.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,5 @@ var cognome = prompt('Qual è il tuo cognome?');
 var colorePreferito = prompt('Qual è il tuo colore preferito?');
 var annoCorrente = ('19');
 
-// Comandi di debug per visualizzare che tutto funzioni correttamente
-console.log(nome);
-console.log(cognome);
-console.log(colorePreferito);
-console.log(annoCorrente);
-//Fine comandi di debug
-
 // Output cambiando HTML per visualizzazione nel browser
 document.getElementById('password').innerHTML = nome + cognome + colorePreferito + annoCorrente;
